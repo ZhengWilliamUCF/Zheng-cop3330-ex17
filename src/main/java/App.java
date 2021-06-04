3e4352d598772a1b9d42a7677b9c5fa254840370
William <ZhengWilliam@knights.ucf.edu>
2021-06-04 13:50:28 -0400

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,72 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solutions
+ *  Copyright 2021 William Zheng
+ */
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class App {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        App myApp = new App();
+        //double userWeight = myApp.readUserWeight();
+        String userGender = myApp.readUserGender();
+        //String userConsumption = myApp.readUserConsumption();
+        //String userTime = myApp.readUserTime();
+        double Weight = myApp.readUserWeight();//Double.parseDouble(userWeight);
+        double ADR = myApp.GenderCheck(userGender);
+        double Consumption = myApp.readUserConsumption();//Double.parseDouble(userConsumption);
+        double Hours = myApp.readUserTime();//Double.parseDouble(userTime);
+        String displayMessage = myApp.BACCheck(Consumption, Weight, ADR, Hours);
+        String outputString = myApp.generateOutputString(displayMessage);
+        myApp.printOutput(outputString);
+    }
+
+    public void printOutput(String outputString) {
+        System.out.println(outputString);
+    }
+
+    public double readUserWeight(){
+        System.out.print("What is your weight in pounds? ");
+        return in.nextDouble();
+
+    }
+
+    public String readUserGender(){
+        System.out.print("What is your gender? (Please enter M or F) ");
+        return in.nextLine();
+    }
+
+    public double readUserConsumption(){
+        System.out.print("How many ounces of alcohol did you consume? ");
+        return in.nextDouble();
+    }
+
+    public double readUserTime(){
+        System.out.print("How long has it been since your last drink? ");
+        return in.nextDouble();
+    }
+
+    public String BACCheck(Double Consumption, Double Weight, Double ADR, Double Hours){
+        double BAC = ((Consumption * 5.14)/(Weight * ADR)) - (0.015 * Hours);
+        return (BAC > 0.08) ? String.format("Your BAC is %s.\nIt is not legal for you to drive.", BAC) : String.format("Your BAC is %s.\nIt is legal for you to drive.", BAC);
+    }
+    public double GenderCheck(String userGender){
+        if (userGender.equals("M")){
+            return 0.73;
+        }
+        else if (userGender.equals("F")){
+            return 0.66;
+        }
+        else {
+            System.out.print("Please enter M or F");
+            return GenderCheck(userGender);
+        }
+    }
+
+    public String generateOutputString(String displayMessage){
+        return displayMessage;
+    }
+}
